c45380d3fe99c8b10f874dae3832be17f071f2e9
Yacine KHATIR <yacine77k@gmail.com>
2023-09-12 10:23:15 +0200

    answer to Q1

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -2,6 +2,14 @@ package com.github.forax.framework.mapper;
 
 public final class JSONWriter {
   public String toJSON(Object o) {
-    throw new UnsupportedOperationException("TODO");
+
+    return switch(o) {
+      case null -> "null";
+      case String s -> '"'+String.valueOf(s)+'"';
+      default -> String.valueOf(o);
+
+    };
+
+
   }
 }
